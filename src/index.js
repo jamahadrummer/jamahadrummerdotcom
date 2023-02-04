a95979822d92befa7cbe7e42a26b19fa958596e2
jamahadrummer <57222656+jamahadrummer@users.noreply.github.com>
2023-02-04 13:41:46 -0600

    id fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
-const btn5 = document.getElementById('btn4');
-const btn6 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
 
 const pageTurn = (pageName) => {
     location.href = pageName;
